c15f547fe15dd671aa0308a2c9d971e2babcef3b
joserosalesdh <jm.rosales92@gmail.com>
2020-09-08 13:38:19 -0300

    Logout del user

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,11 +1,21 @@
 import React, { useContext } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useHistory } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
+
 
 
 export const Navbar = () => {
 
-    const { user: { name } } = useContext(AuthContext);
+    const { user: { name }, dispatch } = useContext(AuthContext);
+    const history = useHistory(); //esto ya me da la history y no hace falta que la mande desde el dashboardroute
+    // El context provider facilita información en el arbol de componentes 
+    const handleLogout = () => {
+        dispatch({
+            type: types.logout
+        });
+        history.replace('/login')
+    };
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -54,14 +64,12 @@ export const Navbar = () => {
                     <span className="nav-item nav-link text-info">
                         {name}
                     </span>
-                    <NavLink
-                        activeClassName="active"
-                        className="nav-item nav-link"
-                        exact
-                        to="/login"
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
